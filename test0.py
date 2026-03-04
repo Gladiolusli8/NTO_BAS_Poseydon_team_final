841ac31b68ff9ea0a1021a2268adfd667fa73dd5
gladiolus_li8 <sapr03062010@mail.ru>
2026-03-04 14:36:03 +0400

    test 0 na proverku

--- test0.py
+++ test0.py
@@ -4,16 +4,17 @@ import math
 drun = Pioneer()
 def wait_for_state(drone, target_state, timeout=30):
     start = time.time()
-    while drone.get_autopilot_state() != target_state:
-        if time.time() - start > timeout:
-            return False
-        time.sleep(0.1)
+    if drone.get_autopilot_state() != target_state:
+        return False
     return True
-drun.connection.wait_heartbeat(timeout=10)
-drun.connection.mav.request_data_stream(1, 30, 10, 1)
-drun.arm()
-drun.takeoff()
-
-
-drun.land()
-drun.disarm()
+try:
+    drun.arm()
+    drun.takeoff()
+    while not wait_for_state(drun, "MISSION"):
+        time.sleep(0.1)
+    time.sleep(10)
+    drun.land()
+    drun.disarm()
+except:
+    drun.land()
+    drun.disarm()
