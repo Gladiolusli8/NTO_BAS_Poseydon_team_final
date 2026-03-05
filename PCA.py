0ba95741aa7a9e6b6fd56af229aa60b1816f9177
IvanS297 <ivan.ser202@gmail.com>
2026-03-05 13:51:25 +0700

    обновил pca

--- PCA.py
+++ PCA.py
@@ -10,27 +10,21 @@ def drawAxis(img, p_, q_, colour, scale):
     p = list(p_)
     q = list(q_)
 
-    angle = atan2(p[1] - q[1], p[0] - q[0])  # angle in radians
-    hypotenuse = sqrt((p[1] - q[1]) * (p[1] - q[1]) +
-                      (p[0] - q[0]) * (p[0] - q[0]))
-
+    angle = atan2(p[1] - q[1], p[0] - q[0])  # angle from q to p
+    hypotenuse = sqrt((p[1] - q[1])**2 + (p[0] - q[0])**2)
     # Here we lengthen the arrow by a factor of scale
     q[0] = p[0] - scale * hypotenuse * cos(angle)
     q[1] = p[1] - scale * hypotenuse * sin(angle)
-    cv.line(img, (int(p[0]), int(p[1])),
-            (int(q[0]), int(q[1])), colour, 1, cv.LINE_AA)
+    cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv.LINE_AA)
 
     # create the arrow hooks
     p[0] = q[0] + 9 * cos(angle + pi / 4)
     p[1] = q[1] + 9 * sin(angle + pi / 4)
-    cv.line(img, (int(p[0]), int(p[1])),
-            (int(q[0]), int(q[1])), colour, 1, cv.LINE_AA)
+    cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv.LINE_AA)
 
     p[0] = q[0] + 9 * cos(angle - pi / 4)
     p[1] = q[1] + 9 * sin(angle - pi / 4)
-    cv.line(img, (int(p[0]), int(p[1])),
-            (int(q[0]), int(q[1])), colour, 1, cv.LINE_AA)
-
+    cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv.LINE_AA)
 
 def getOrientation(pts, img):
 
@@ -57,43 +51,57 @@ def getOrientation(pts, img):
 
     # orientation in radians
     angle = atan2(eigenvectors[0, 1], eigenvectors[0, 0])
-
+    
     return angle
 
+def get_angle_pca(image_path):
+    img = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
+    contours, _ = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    if not contours:
+        return None
+    
+    largest_contour = max(contours, key=cv.contourArea)
+    points = largest_contour.reshape(-1, 2).astype(np.float32)
+    mean, eigenvectors = cv.PCACompute(points, mean=None)
+    main_direction = eigenvectors[0]
+    angle = np.arctan2(main_direction[1], main_direction[0]) * 180 / np.pi
+    return angle
 
-#parser = argparse.ArgumentParser(description='Code for Introduction to Principal Component Analysis (PCA) tutorial.\
-#                                              This program demonstrates how to use OpenCV PCA to extract the orientation of an object.')
-#parser.add_argument('--input', help='Path to input image.',
-#                    default='photo.jpeg')
-#args = parser.parse_args()
+def draw_main_axis(img, contour, color=(0, 255, 0), length=100):
+    points = contour.reshape(-1, 2).astype(np.float32)
+    
+    mean, eigenvectors = cv.PCACompute(points, mean=None)
+    center = (int(mean[0, 0]), int(mean[0, 1]))
+    
+    right_end = (int(center[0] + length), int(center[1]))
+    cv.line(src, (int(center[0]), int(center[1])), right_end, (255, 255, 255), 2)
+    
+    return center
 
-src = cv.imread('photo.jpeg')
-# Check if image is loaded successfully
+src = cv.imread('drone.png')
 if src is None:
     print('Could not open or find the image: ', src)
     exit(0)
 
 cv.imshow('src', src)
 
-# Convert image to grayscale
 gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-
-# Convert image to binary
 _, bw = cv.threshold(gray, 50, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
 
 contours, _ = cv.findContours(bw, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
 
 for i, c in enumerate(contours):
-    # Calculate the area of each contour
     area = cv.contourArea(c)
-    # Ignore contours that are too small or too large
     if area < 1e2 or 1e5 < area:
         continue
 
-    # Draw each contour only for visualisation purposes
     cv.drawContours(src, contours, i, (0, 0, 255), 2)
-    # Find the orientation of each shape
-    getOrientation(c, src)
+    src1 = src.copy()
+    angle = getOrientation(c, src)
+    angle1 = get_angle_pca('drone.png')
+    print(angle)
+    print(angle1)
+    draw_main_axis(src, c)
 
 cv.imshow('output', src)
 cv.waitKey()
