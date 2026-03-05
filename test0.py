4fdcd939a0e39984f91dd6750f767ce9826bcb61
Roman <r9032813111@gmail.com>
2026-03-05 09:30:46 +0400

    Поднимаем дрон на метр. потом сажаем. предусмотренна принудительная посадка с ноута

--- test0.py
+++ test0.py
@@ -7,9 +7,12 @@ try:
     pioneer.go_to_local_point(x=0, y=0, z=1, yaw=0)
     while not pioneer.point_reached():
         time.sleep(0.1)
+    time.sleep(10)
     pioneer.land()
     pioneer.disarm()
 except KeyboardInterrupt:
     print("Остановка программы, производится посадка")
     pioneer.land()
+    time.sleep(3)
+    pioneer.disarm()
 pioneer.close_connection()
